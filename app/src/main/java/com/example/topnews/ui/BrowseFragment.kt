c8245cfd20ef54d6c64dc04dc525f1eaa5ce21c8
eunjegal <eunjegal@umd.edu>
2023-02-25 15:04:42 -0500

    Set up each fragment

--- app/src/main/java/com/example/topnews/ui/BrowseFragment.kt
+++ app/src/main/java/com/example/topnews/ui/BrowseFragment.kt
@@ -1,7 +1,18 @@
 package com.example.topnews.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.topnews.R
 
 class BrowseFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_browse, container, false)
+    }
 }
--- app/src/main/java/com/example/topnews/ui/MainActivity.kt
+++ app/src/main/java/com/example/topnews/ui/MainActivity.kt
@@ -21,10 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_feed, R.id.navigation_browse, R.id.navigation_saved
